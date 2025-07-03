262f88142a06e77c5f09ad820bc10bf98c3c1c6a
An0nman <03334121a@gmail.com>
2025-07-02 21:59:48 -0700

    t2

--- t2/script.js
+++ t2/script.js
@@ -1,6 +1,7 @@
 let page_content = "";
-function createContent(one, two, three, four) {
-    page_content = `<img src="img/1.jpg" alt="" />
+function createContent(one, two, three, four, file) {
+  page_content = `
+        <img src="img/1.jpg" alt="" />
         <img src="img/2.jpg" alt="" />
         <img src="img/3.jpg" alt="" />
         <img src="img/4.jpg" alt="" />
@@ -9,7 +10,7 @@ function createContent(one, two, three, four) {
         <div class="blackness"></div>
     
         <div class="content">
-          <h1>This is the main page</h1>
+          <h1>This is the ${file} page</h1>
     
           <div style="display: flex; gap: 20px; margin-bottom: 20px">
             <a href="https://www.google.com" target="_blank">Google</a>
@@ -26,5 +27,23 @@ function createContent(one, two, three, four) {
             <a href="${four}.html">${four} Page</a>
           </div>
         </div>`
+  document.querySelector("body").innerHTML = page_content;
 }
-document.querySelector("body").innerHTML = page_content;
+let file = document.querySelector(".file").textContent;
+switch (file) {
+  case '0':
+    createContent("First", "Second", "Third", "Fourth", "Main");
+    break;
+  case '1':
+    createContent("Main", "Second", "Third", "Fourth", "First");
+    break;
+  case '2':
+    createContent("Main", "First", "Third", "Fourth", "Second");
+    break;
+  case '3':
+    createContent("Main", "First", "Second", "Fourth", "Third");
+    break;
+  case '4':
+    createContent("Main", "First", "Second", "Third", "Fourth");
+    break;
+}
